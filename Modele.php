9364e9ae2533b0ebafd3e90f972ac56acd6a9749
ghizlane-aspic <ghizlanetouguii@gmail.com>
2025-11-28 12:25:19 +0100

    gestion de billets

--- Modele.php
+++ Modele.php
@@ -1,11 +1,32 @@
 <?php
-// Renvoie la liste de tous les billets, triés par identifiant décroissant
-function getBillets() { 
+// Renvoie les informations sur un billet
+function getBillet($idBillet) {
 $bdd = getBdd();
-$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+$billet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
 . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-. ' order by BIL_ID desc');
-return $billets;
+. ' where BIL_ID=?');
+$billet->execute(array($idBillet));
+if ($billet->rowCount() == 1)
+return $billet->fetch(); // Accès à la première ligne de résultat
+else
+throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
+}
+
+// Renvoie la liste de tous les billets
+function getBillets() {
+$bdd = getBdd();
+$resultat = $bdd->query('select BIL_ID as id, BIL_DATE as date, BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET order by BIL_ID desc');
+return $resultat->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// Renvoie la liste des commentaires associés à un billet
+function getCommentaires($idBillet) {
+$bdd = getBdd();
+$commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
+. ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
+. ' where BIL_ID=?');
+$commentaires->execute(array($idBillet));
+return $commentaires;
 }
 // Effectue la connexion à la BDD
 // Instancie et renvoie l'objet PDO associé
--- vueAccueil.php
+++ vueAccueil.php
@@ -7,7 +7,9 @@ $titre = 'Mon Blog';
 <?php foreach ($billets as $billet): ?>
     <article>
         <header>
+            <a href="<?= "billet.php?id=" . $billet['id'] ?>">
             <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+            </a>
             <time><?= $billet['date'] ?></time>
         </header>
         <p><?= $billet['contenu'] ?></p>
