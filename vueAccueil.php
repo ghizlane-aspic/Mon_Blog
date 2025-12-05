8257506aafd5e5b44ea8e5a2ca4bcdffdf3897cc
ghizlane-aspic <ghizlanetouguii@gmail.com>
2025-12-05 12:20:00 +0100

    mvc4

--- vueAccueil.php
+++ vueAccueil.php
@@ -8,8 +8,8 @@ $titre = 'Mon Blog';
 
     <article>
         <header>
-          <a href="<?= "index.php?action=billet&id=" . $billet['id'] ?>">
-            <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+            <a href="<?= "billet.php?id=" . $billet['id'] ?>">
+              <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
             </a>
             <time><?= $billet['date'] ?></time>
         </header>
